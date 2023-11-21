ba9ec401c4acd52c9271d7f2b13d4ea45a01daac
o-rchid <ozzve35@gmail.com>
2023-11-21 23:37:14 +0900

    JdbcTemplate 적용

---
+++ src/main/java/hello/itemservice/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -0,0 +1,32 @@
+package hello.itemservice.jdbctemplate;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
+
+    @Override
+    public Item save(Item item) {
+        return null;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return null;
+    }
+}
